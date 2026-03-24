4f5180548ec3f516b57102b3b49b6d703193bd03
konstantin <konstantin.klein@hochfrequenz.de>
2026-03-24 16:05:48 +0100

    refactor: move transport XML types to adtmodel/

---
+++ adtmodel/transport.go
@@ -0,0 +1,24 @@
+package adtmodel
+
+import "encoding/xml"
+
+// TransportRoot is the XML response from GET /sap/bc/adt/cts/transportrequests.
+type TransportRoot struct {
+	XMLName           xml.Name              `xml:"root"`
+	WorkbenchRequests []TransportRequestXML `xml:"workbenchRequests>workbenchRequest"`
+}
+
+// TransportRequestXML is a single transport request in the transport list XML response.
+type TransportRequestXML struct {
+	Number      string `xml:"number,attr"`
+	Owner       string `xml:"owner,attr"`
+	Description string `xml:"shortDescription,attr"`
+	Status      string `xml:"status,attr"`
+}
+
+// TransportComponent is the XML body for adding an object to a transport.
+type TransportComponent struct {
+	XMLName   xml.Name `xml:"adtcore:objectReference"`
+	NSCore    string   `xml:"xmlns:adtcore,attr"`
+	ObjectURI string   `xml:"adtcore:uri,attr"`
+}
