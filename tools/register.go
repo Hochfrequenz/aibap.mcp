f761d3adad372fa8ae4ea4e3998bfc4cb288ea36
Matthias-René Dachner <matthias-rene.dachner@hochfrequenz.de>
2026-04-03 08:53:38 +0200

    feat(#162): add short dump viewer (ST22) (#196)

--- tools/register.go
+++ tools/register.go
@@ -71,4 +71,5 @@ func RegisterAllWithLockMap(s *server.MCPServer, client adt.Client, selector Sys
 	registerTextElementTools(ls, client)
 	registerMessageClassTools(ls, client)
 	registerEnhancementTools(ls, client)
+	registerShortDumpTools(ls, client)
 }
